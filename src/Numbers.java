dc34748e7f157d1ff9d7fa6aded0d75586a14216
Joseph Carrillo <98797753+JaayCarrillo@users.noreply.github.com>
2023-05-25 00:40:58 -0700

    Numbers.java class update : 4. section a -c(i-iii)

--- src/Numbers.java
+++ src/Numbers.java
@@ -10,19 +10,24 @@ public class Numbers {
         this.randomNum = randomNum;
     }
     public void generateNumber() {
-        /*int min = 0;
-        int max = 100;
-
-        int random = (int)Math.floor(Math.random() * (max-min + 1) + min);
-        int random = randomNum;
-        System.out.println(random);
-
-        int random = randomNum.nextInt(100);
-
-         */
 
         Random random = new Random();
         int randomNum = random.nextInt(100);
         System.out.println(randomNum);
+    }
+
+    public boolean compareNumber(int guess){
+        if (guess == randomNum){
+            System.out.println("Congratulations, you guess the number!");
+            return true;
+        }
+        if( guess > randomNum){
+            System.out.println("I'm sorry. That guess was too high.");
+            return false;
+        }
+        if(guess < randomNum){
+            System.out.println("I'm sorry, That guess was too low.");
+        }
+        return false;
     }
 }
