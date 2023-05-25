5a994347c0a0121c9e58d4f27e92a711a34c10ce
Joseph Carrillo <98797753+JaayCarrillo@users.noreply.github.com>
2023-05-25 01:23:05 -0700

    Game Play Update stuck on section C of the first if statement. Working through a solution

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,5 +1,19 @@
-public class GamePlay {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class GamePlay {
+
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+        System.out.println("Would you like to enter a last name? ");
+
+        if(input.equals("yes")){
+            System.out.println("enter last name: ");
+            String lastName = scanner.nextLine();
+            Person person = new Person(input, lastName);
+            System.out.println(person);
+        }
     }
 }
